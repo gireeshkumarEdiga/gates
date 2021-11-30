f21c146e97a6159ac77f880bf6d6b024d5000e60
Gireeshkumar Ediga <egireeshkumar99@gmail.com>
2021-11-30 22:08:00 +0530

    gates

---
+++ gsi (3).js
@@ -0,0 +1,65 @@
+function gatesStudentsIkkada() 
+{
+	window.open("gatesstudentsikkada.html")
+}
+
+function gatesWebPage()
+{
+	window.open("http://gatesit.ac.in/")
+}
+
+function gatesEcap() 
+{
+	window.open("http://webprosindia.com/gates/")
+}
+function gatesWebPage()
+{
+	window.open("http://gatesit.ac.in/")
+}
+
+function studyMaterial()
+{
+	window.open("gsi study.html")
+}
+	function jntuaResults() 
+{
+	window.open("https://jntuaresults.ac.in/")
+}
+
+function notifications() 
+{
+	window.open("https://www.jntua.ac.in/category/examinations-notifications/")
+}
+	function gsiInstagram() 
+{
+	window.open("https://www.instagram.com/gates_students_ikkada/")
+}
+	//.......................................................
+function civil() 
+{
+	window.open("civil.html")
+}
+	function cse() 
+{
+	window.open("cse.html")
+}
+function ds() 
+{
+	window.open("ds.html")
+}
+	function ece() 
+{
+	window.open("ece.html")
+}
+	function eee() 
+{
+	window.open("eee.html")
+}
+	function ai() 
+{
+	window.open("ai.html")
+}
+function me() 
+{
+	window.open("me.html")
+}
